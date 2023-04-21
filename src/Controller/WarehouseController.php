ed15b4fbffd55c2296fcd433b4308f5f87ad02cb
W. Gawroński <wgawronski.wg@gmail.com>
2023-04-21 08:27:32 +0200

    Fix: Warehouse: available for logged users

--- src/Controller/WarehouseController.php
+++ src/Controller/WarehouseController.php
@@ -9,8 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[Route('/dashboard/warehouse')]
+#[Route('/dashboard/warehouse'), IsGranted("ROLE_USER") ]
 class WarehouseController extends AbstractController
 {
     #[Route('/', name: 'app_warehouse_index', methods: ['GET'])]
