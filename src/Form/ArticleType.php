793296da87eff5c5bcec472bf639e05a483da80d
W. Gawroński <wgawronski.wg@gmail.com>
2023-04-28 11:52:23 +0200

    Fix: Article: Form - not blank name, Index: link to file

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,19 +3,27 @@
 namespace App\Form;
 
 use App\Entity\Article;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'required' => true,
+                'constraints'=> [
+                    new NotBlank()
+                ],
+            ])
             ->add('vat')
             ->add('file', FileType::class, [
                 'label' => 'File | Only .pdf, max-size: 2M',
